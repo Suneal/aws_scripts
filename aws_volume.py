87a8b22322b4a1ba5ef518ca99d2182445e52d40
Sunil <syncsynergy.sunil@gmail.com>
2017-06-10 16:36:19 -0400

    fetch region from the instance

--- aws_volume.py
+++ aws_volume.py
@@ -2,9 +2,11 @@ import boto3
 import urllib2
 
 ec2 = boto3.client('ec2')
-volume_response = ec2.create_volume(Size=10, AvailabilityZone='us-east-1a', VolumeType='gp2');
-instanceid = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
 
+instanceid = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+region = urllib2.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone/').read().decode()
+string_region = region.encode('utf-8')
+volume_response = ec2.create_volume(Size=10, AvailabilityZone=string_region, VolumeType='gp2');
 attach_volume_response = ec2.attach_volume(VolumeId=volume_response['VolumeId'],InstanceId=instanceid,Device='/dev/xvdh')
 
 # lsblk
