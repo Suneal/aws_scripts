e70158ce5187beabc837c261cd0061f3bc090009
Sunil <syncsynergy.sunil@gmail.com>
2017-06-11 14:09:32 -0400

    minor typo fix

--- terminate.py
+++ terminate.py
@@ -6,9 +6,9 @@ import time
 ec2 = boto3.client('ec2')
 instanceid = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
 desc_instance = ec2.describe_instances(InstanceIds=[instanceid])
-spot_request_id= instancedes['Reservations'][0]['Instances'][0]['SpotInstanceRequestId']
-cancel_spot_request = client.cancel_spot_instance_requests(SpotInstanceRequestIds=[spot_request_id,])
-terminate_instance = client.terminate_instances(InstanceIds=[instanceid,])
+spot_request_id= desc_instance['Reservations'][0]['Instances'][0]['SpotInstanceRequestId']
+cancel_spot_request = ec2.cancel_spot_instance_requests(SpotInstanceRequestIds=[spot_request_id,])
+terminate_instance = ec2.terminate_instances(InstanceIds=[instanceid,])
 
 
 
