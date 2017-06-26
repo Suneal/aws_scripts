efc8fda0b1cc16e51b3abab9a2bb7f5080e9e334
Sunil <syncsynergy.sunil@gmail.com>
2017-06-26 16:33:53 -0400

    Instance id change for Amazon Linux

--- aws_volume.py
+++ aws_volume.py
@@ -1,12 +1,17 @@
 import boto3
 import urllib2
 import time
-
+import commands
 ec2 = boto3.client('ec2')
 
 ip_address= str(urllib2.urlopen('http://ip.42.pl/raw').read())
 
-instanceid = urllib2.urlopen('http://' + ip_address + '/latest/meta-data/instance-id').read().decode()
+#This didn't work for amazon instance
+# instanceid = urllib2.urlopen('http://' + ip_address + '/latest/meta-data/instance-id').read().decode()
+
+#For Amazon Linux
+instanceid = commands.getstatusoutput('ec2-metadata --instance-id | cut -d " " -f 2')[1]
+
 region = urllib2.urlopen('http://' + ip_address + '/latest/meta-data/placement/availability-zone/').read().decode()
 string_region = region.encode('utf-8')
 volume_response = ec2.create_volume(Size=10, AvailabilityZone=string_region, VolumeType='gp2');
