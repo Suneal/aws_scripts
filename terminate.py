75679df150ab9c54d0fe27af8a8057a0ea0c756e
Sunil <syncsynergy.sunil@gmail.com>
2017-06-27 09:23:24 -0400

    terminate instanceid

--- terminate.py
+++ terminate.py
@@ -4,7 +4,8 @@ import time
 
 #Cancels spot request and terminates instance
 ec2 = boto3.client('ec2')
-instanceid = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+instanceid = commands.getstatusoutput('ec2-metadata --instance-id | cut -d " " -f 2')[1]
+#instanceid = urllib2.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
 desc_instance = ec2.describe_instances(InstanceIds=[instanceid])
 spot_request_id= desc_instance['Reservations'][0]['Instances'][0]['SpotInstanceRequestId']
 cancel_spot_request = ec2.cancel_spot_instance_requests(SpotInstanceRequestIds=[spot_request_id,])
